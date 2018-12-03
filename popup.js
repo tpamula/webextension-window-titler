128b76823ae04e30f59c2469373bb296fe99dcbc
Tomasz Pamula <tpamula@users.noreply.github.com>
2018-12-03 12:51:11 +0000

    Initial version.

---
+++ popup.js
@@ -0,0 +1,16 @@
+class WindowTitleManager {
+	async rename(title) {
+		let computedTitle = title ? `[${title}] ` : '';
+		
+		let currentWindow = await window.browser.windows.getCurrent();
+		browser.windows.update(currentWindow.id, { titlePreface: computedTitle });
+	}
+}
+
+document.getElementById("window-title-prefix-form").addEventListener("submit", (e) => {
+	let title = document.getElementById("title-input").value;
+
+	new WindowTitleManager().rename(title);
+
+	e.preventDefault();
+});
