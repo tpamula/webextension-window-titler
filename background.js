4703ed7f92ecd8e7efdd4a828769a0c9993e2276
Tomasz Pamula <tpamula@users.noreply.github.com>
2019-02-24 13:12:47 +0000

    Update the window titles when a session is restored from the session manager.

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
 function restoreWindowTitles() {
   browser.windows.getAll((windows) => {
     windows.map(window => window.id).forEach((windowId) => {
-      browser.sessions.getWindowValue(windowId, 'title')
-        .then(title => browser.windows.update(windowId, { titlePreface: title }));
+      browser.sessions.getWindowValue(windowId, 'title').then((title) => {
+        if (title) browser.windows.update(windowId, { titlePreface: title });
+      });
     });
   });
 }
 
+// Needs to listen in case the user restores windows by clicking the restore button in the session
+// manager window.
+// http://kb.mozillazine.org/Browser.sessionstore.max_resumed_crashes
+//
+// There doesn't seem to be an appropriate event firing after the session is restored so resorting
+// to this one instead.
+browser.tabs.onCreated.addListener(() => {
+  restoreWindowTitles();
+});
+
+// Needs to run if the session is restored automatically, without the session manager window.
 restoreWindowTitles();
